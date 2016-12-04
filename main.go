12be798eddb9353cf0c8856a2ba6d9a10f82ebff
Alban Crequy <alban@kinvolk.io>
2016-12-04 19:12:59 +0100

    sort perf events with timestamps
    BPF programs are executed in kernel concurrently and each cpu stores
    perf events in its own ring buffer. In userspace, we want to read from
    all the ring buffers in order. To achieve this, this patch adds the
    following:
    
    - Timestamp in each perf event. Timestamps must be at the beginning of
      the struct.
    
    - Harvest the events from all ring buffers into an "incoming" array.
      After an harvest round, sort them by timestamp. Events from different
      ring buffer might be collected in the wrong order.
    
    - Events are still being received during the ring buffer harvest. To
      avoid the race when events from different harvest round are unordered,
      don't process events timestamped after the beginning of the harvest.

--- main.go
+++ main.go
@@ -1,62 +1,78 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
+	"os/signal"
 	"unsafe"
 
 	bpf "github.com/kinvolk/go-ebpf-kprobe-example/bpf"
 )
 
-/*
-#cgo CFLAGS: -Wall -Wno-unused-variable
-
-#include <stdlib.h>
-#include <stdint.h>
-//#include <sys/ioctl.h>
-//#include <linux/perf_event.h>
-#include <poll.h>
-#include <errno.h>
-//#include <linux/bpf.h>
-
-#define TASK_COMM_LEN 16
-
-struct tcp_event_t {
-    char ev_type[12];
-    uint32_t pid;
-    char comm[TASK_COMM_LEN];
-    uint32_t saddr;
-    uint32_t daddr;
-    uint16_t sport;
-    uint16_t dport;
-    uint32_t netns;
-};
-*/
 import "C"
 
-//export tcpEventCb
-func tcpEventCb(data []byte) {
-	tcpEvent := (*C.struct_tcp_event_t)(unsafe.Pointer(&data[0]))
+type tcpEvent struct {
+	// Timestamp must be the first field, the sorting depends on it
+	Timestamp uint64
 
-	typ := C.GoString(&tcpEvent.ev_type[0])
-	pid := tcpEvent.pid & 0xffffffff
+	Cpu   uint64
+	Type  [12]C.char
+	Pid   uint32
+	Comm  [16]C.char
+	SAddr uint32
+	DAddr uint32
+	SPort uint16
+	DPort uint16
+	NetNS uint32
+}
+
+var byteOrder binary.ByteOrder
+
+// In lack of binary.HostEndian ...
+func init() {
+	var i int32 = 0x01020304
+	u := unsafe.Pointer(&i)
+	pb := (*byte)(u)
+	b := *pb
+	if b == 0x04 {
+		byteOrder = binary.LittleEndian
+	} else {
+		byteOrder = binary.BigEndian
+	}
+}
+
+var lastTimestamp uint64
+
+func tcpEventCb(event tcpEvent) {
+	timestamp := uint64(event.Timestamp)
+	cpu := event.Cpu
+	typ := C.GoString(&event.Type[0])
+	pid := event.Pid & 0xffffffff
 
 	saddrbuf := make([]byte, 4)
 	daddrbuf := make([]byte, 4)
 
-	binary.LittleEndian.PutUint32(saddrbuf, uint32(tcpEvent.saddr))
-	binary.LittleEndian.PutUint32(daddrbuf, uint32(tcpEvent.daddr))
+	binary.LittleEndian.PutUint32(saddrbuf, uint32(event.SAddr))
+	binary.LittleEndian.PutUint32(daddrbuf, uint32(event.DAddr))
 
 	sIP := net.IPv4(saddrbuf[0], saddrbuf[1], saddrbuf[2], saddrbuf[3])
 	dIP := net.IPv4(daddrbuf[0], daddrbuf[1], daddrbuf[2], daddrbuf[3])
 
-	sport := tcpEvent.sport
-	dport := tcpEvent.dport
-	netns := tcpEvent.netns
+	sport := event.SPort
+	dport := event.DPort
+	netns := event.NetNS
 
-	fmt.Printf("%s %v %v:%v %v:%v %v\n", typ, pid, sIP, sport, dIP, dport, netns)
+	fmt.Printf("%v cpu#%d %s %v %v:%v %v:%v %v\n", timestamp, cpu, typ, pid, sIP, sport, dIP, dport, netns)
+
+	if lastTimestamp > timestamp {
+		fmt.Printf("WARNING: late event!\n")
+		os.Exit(1)
+	}
+
+	lastTimestamp = timestamp
 }
 
 func main() {
@@ -69,7 +85,26 @@ func main() {
 	}
 
 	fmt.Printf("Ready.\n")
-	b.Poll("tcp_event", tcpEventCb)
 
-	select {}
+	channel := make(chan []byte)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, os.Kill)
+
+	go func() {
+		var event tcpEvent
+		for {
+			data := <-channel
+			err := binary.Read(bytes.NewBuffer(data), byteOrder, &event)
+			if err != nil {
+				fmt.Printf("failed to decode received data: %s\n", err)
+				continue
+			}
+			tcpEventCb(event)
+		}
+	}()
+
+	b.PollStart("tcp_event", channel)
+	<-sig
+	b.PollStop("tcp_event")
 }
